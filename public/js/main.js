7e3509c9f4d0a0269173df3e5f0286e667084494
Abdellah <mounajaikabdellah@gmail.com>
2021-01-31 01:35:49 +0100

    exo_fini

---
+++ public/js/main.js
@@ -0,0 +1,74 @@
+// exo1
+// let prom = prompt("couleur préféré?")
+// while (prom != "bleu") {
+//     prom = prompt("re tente?")
+// }
+
+// let nombre = prompt("nombre?")
+// while (nombre < 10) {
+//     nombre = prompt("re tente?")
+// }
+
+// exo2
+
+// let compt = 0;
+// while (compt <= 11) {
+//     if (compt % 2 == 1) {
+//         console.log(compt);
+//     };
+//     compt++
+// }
+
+// let prenom = prompt("prenom?")
+// prenomTaille = prenom.length
+// let compt = 0
+
+// while (compt < prenomTaille) {
+//     alert(prenom)
+//     compt++
+// }
+
+// let compt = 0
+// let prenom = prompt("prenom?")
+// prenomTaille = prenom.length
+// while (compt < prenomTaille) {
+//     alert(prenom.charAt(compt))
+//     compt++
+// }
+
+// exo3
+
+// let prenom = prompt("prenom?")
+// let compt = 0
+// let prenomTaille = prenom.length
+// while (compt < prenomTaille) {
+//     if (compt % 2 == 1) {
+//         alert(prenom.charAt(compt).toLocaleUpperCase())
+//     } else {
+//         alert(prenom.charAt(compt).toLowerCase())
+//     }
+//     compt++
+// }
+
+// let calcul = prompt("rentre un calcul")
+
+// while (condition) {
+//     if (condition) {
+        
+//     } else {
+        
+//     }
+// }
+
+// let prenom = prompt("prenom?")
+// while (prenom.length < 5) {
+//         prenom = prompt("trop court, prenom?")
+//         if (prenom.length > 5) {
+//             alert(prenom.toLocaleUpperCase())
+//         }              
+// }
+
+// programme qui affiche chaque lettre du prénom MAIS à chaque tour impair la lettre sera en majuscule
+// Créer un programme qui demande d'entrer un calcul, laisse le choix a l'utilisateur , exemple 3,5 * 3 et si la réponse est a virgule il faut afficher le résultat au nom entier le plus haut
+// Créer un programme qui vérifie la longueur du prénom qui doit etre plus grand que 5 caractères et qui doit etre tout en minuscule, une fois que les conditions sont respectés, afficher le prénom en majuscule
+
